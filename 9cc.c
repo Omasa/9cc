b125d4b94ccaaee4f7a45bf42821cccd543ee260
okubo <tkgool000@yahoo.co.jp>
2021-05-03 16:31:35 +0900

    インデント修正

--- 9cc.c
+++ 9cc.c
@@ -78,16 +78,16 @@ void error_at(char *loc,char *fmt, ...){
 //真を返す。それ以外の場合には偽を返す。
 bool consume(char *op){
 	if(token->kind != TK_RESERVED || 
-		strlen(op) != token->len ||
-		memcmp(token->str,op,token->len))
+			strlen(op) != token->len ||
+			memcmp(token->str,op,token->len))
 		return false;
 	token = token->next;
 	return true;
 }
 void expect(char *op){
 	if(token->kind != TK_RESERVED ||
-		strlen(op) != token->len ||
-		memcmp(token->str,op,token->len))
+			strlen(op) != token->len ||
+			memcmp(token->str,op,token->len))
 		error_at(token->str, "expected '%c'",op);
 	token = token->next;
 }
@@ -129,12 +129,12 @@ Token *tokenize(){
 
 	while (*p){
 		if(isspace(*p)){
-		p++;
-		continue;
+			p++;
+			continue;
 		}
 		//複数文字
 		if(startswith(p, "==") || startswith(p, "!=") ||
-			startswith(p,"<=") || startswith(p,">=")){
+				startswith(p,"<=") || startswith(p,">=")){
 			cur = new_token(TK_RESERVED,cur,p,2);
 			p+= 2;
 			continue;
@@ -152,7 +152,7 @@ Token *tokenize(){
 			cur->len = p - q;
 			continue; 
 		}
-	error_at(p, "invalid token");
+		error_at(p, "invalid token");
 	}
 	new_token(TK_EOF,cur,p,0);
 	return head.next;
@@ -311,28 +311,28 @@ void gen(Node *node){
 }
 
 int main (int argc, char **argv){
-		if(argc !=2){
-			error("引数の個数が正しくありません");
-			return 1;
-		}
+	if(argc !=2){
+		error("引数の個数が正しくありません");
+		return 1;
+	}
 
-//トークナイズしてパースする
-user_input = argv[1];
-token = tokenize();
-Node *node =expr();
+	//トークナイズしてパースする
+	user_input = argv[1];
+	token = tokenize();
+	Node *node =expr();
 
 
-////アセンブリの前半部分を出力
+	////アセンブリの前半部分を出力
 	printf(".intel_syntax noprefix\n");
 	printf(".global main \n");
 	printf("main:\n");
 
-//抽象構文木を下りながらコード生成
-gen(node);
+	//抽象構文木を下りながらコード生成
+	gen(node);
 
-// スタックトップに式全体の値が残っているはずなので
-// それをRAXにロードして関数からの返り値とする
-printf("  pop rax\n");
-printf("  ret\n");
-return 0;
+	// スタックトップに式全体の値が残っているはずなので
+	// それをRAXにロードして関数からの返り値とする
+	printf("  pop rax\n");
+	printf("  ret\n");
+	return 0;
 }
